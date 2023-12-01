333974382de50f2fff9838c0dcf3f3d375c99e86
Egor Musiyak <64477831+Egor767@users.noreply.github.com>
2023-12-01 11:13:07 +0100

    Lab1 ex2 egor767 (#2)
    * 1
    
    * .Lib
    
    * .Lib
    
    * add tests
    
    * add tests
    
    * fixes 1
    
    * fixes 2
    
    * fixed AngleTest.cs
    
    * Update test.yml
    
    * Update TurnCommand.cs
    
    * Update ICommand.cs
    
    * Update ITurnable.cs
    
    * fxed
    
    * some changes

---
+++ SpaceBattle.Lib/Commands/TurnCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace SpaceBattle.Lib;
+public class Turn : ICommand
+{
+    private ITurnable _turnable;
+    public Turn(ITurnable turnable)
+    {
+        _turnable = turnable;
+    }
+    public void Execute()
+    {
+        _turnable.Angle += _turnable.AngleVelocity;
+    }
+}
+
---
+++ SpaceBattle.Lib/Interfaces/ICommand.cs
@@ -0,0 +1,7 @@
+namespace SpaceBattle.Lib;
+
+public interface ICommand
+{
+    public void Execute();
+}
+
