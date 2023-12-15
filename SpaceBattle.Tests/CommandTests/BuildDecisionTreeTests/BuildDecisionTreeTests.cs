4918311845a13b665f61ba30fe701dd1693bc377
Egor Musiyak <64477831+Egor767@users.noreply.github.com>
2023-12-15 10:26:05 +0000

    final

--- SpaceBattle.Tests/CommandTests/BuildDecisionTreeTests/BuildDecisionTreeTests.cs
+++ SpaceBattle.Tests/CommandTests/BuildDecisionTreeTests/BuildDecisionTreeTests.cs
@@ -6,9 +6,9 @@ using Hwdtech.Ioc;
 
 namespace SpaceBattle.Tests;
 
-public class BuildDecisionTests
+public class BuildTreeTests
 {
-    public void BuildDecisionTreeTests()
+    public BuildTreeTests()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
