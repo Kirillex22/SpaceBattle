f3b80378dead58f516339e41ee4cf7860f956d78
Kirill <ziabrew34@gmail.com>
2023-12-23 10:46:13 +0000

    fixed

--- SpaceBattle.Lib/Commands/CheckCollisionCommand.cs
+++ SpaceBattle.Lib/Commands/CheckCollisionCommand.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace SpaceBattle.Lib;
 
-public class CheckCollisionCommand
+public class CheckCollisionCommand : ICommand
 {
     private int[]? _state;
     private Func<IDictionary, int, IDictionary>? _searcher;
@@ -13,9 +13,9 @@ public class CheckCollisionCommand
     private IUObject _obj2;
 
     public CheckCollisionCommand(IUObject obj1, IUObject obj2)
-    { 
+    {
         _obj1 = obj1;
-        _obj2 = obj2;     
+        _obj2 = obj2;
     }
 
     public void Execute()
@@ -24,11 +24,11 @@ public class CheckCollisionCommand
             IoC.Resolve<int[]>("Game.UObject.GetProperty", _obj1, "Position"),
             IoC.Resolve<int[]>("Game.UObject.GetProperty", _obj2, "Position")
         };
-        
+
         var velocities = new List<int[]>{
-            IoC.Resolve<int[]>("Game.UObject.GetProperty", _obj1, "Velocity"),      
+            IoC.Resolve<int[]>("Game.UObject.GetProperty", _obj1, "Velocity"),
             IoC.Resolve<int[]>("Game.UObject.GetProperty", _obj2, "Velocity")
-        }; 
+        };
 
         _state = positions[1].ToList().Select((value, index) => value - positions[0][index]).Concat(
                 velocities[1].ToList().Select((value, index) => value - velocities[0][index])
@@ -36,14 +36,15 @@ public class CheckCollisionCommand
 
         var collTree = IoC.Resolve<IDictionary>("Game.Struct.CollisionTree");
 
-        _searcher = (IDictionary currentTree, int parameter) => {
+        _searcher = (IDictionary currentTree, int parameter) =>
+        {
             return currentTree.Contains(parameter) ? (IDictionary)currentTree[parameter] : throw new ArgumentException();
         };
 
         _state.ToList().ForEach(parameter => collTree = _searcher(collTree, parameter));
 
 
-        IoC.Resolve<ICommand>("Game.Event.Collision", _obj1, _obj2).Execute();   
+        IoC.Resolve<ICommand>("Game.Event.Collision", _obj1, _obj2).Execute();
     }
 }
 
