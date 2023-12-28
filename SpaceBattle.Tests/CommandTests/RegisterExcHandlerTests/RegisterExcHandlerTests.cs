1e4a45b73ae071248a91753bf9eb70b1b3be7bcc
Egor Musiyak <64477831+Egor767@users.noreply.github.com>
2023-12-28 10:25:10 +0000

    add tests

---
+++ SpaceBattle.Tests/CommandTests/RegisterExcHandlerTests/RegisterExcHandlerTests.cs
@@ -0,0 +1,40 @@
+using SpaceBattle.Lib;
+using TechTalk.SpecFlow;
+using Moq;
+using Hwdtech;
+using Hwdtech.Ioc;
+
+namespace SpaceBattle.Tests;
+
+public class RegisterExcHandlerTests
+{
+    public RegisterExcHandlerTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        var DecisionTree = new Dictionary<object, object>();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Get.ExceptionTree", (object[] args) => {
+            return DecisionTree;
+        }).Execute();
+    }
+
+    [Fact]
+    public void RegisterExcHandlerSuccess()
+    {
+        var cmd = typeof(Turn);
+        var exc = typeof(ArgumentException);
+        var MoqCommand = new Mock<SpaceBattle.Lib.ICommand>();
+
+        var NewExcHandler = new RegisterExcHandler(cmd, exc, MoqCommand.Object);
+        NewExcHandler.Execute();
+
+        var ExceptionTree = IoC.Resolve<IDictionary<object, object>>("Game.Get.ExceptionTree");
+        Assert.NotNull(ExceptionTree);
+        Assert.True(ExceptionTree.ContainsKey(cmd));
+
+        var NextExceptionTree = (IDictionary<object, object>)ExceptionTree[cmd];
+        Assert.True(NextExceptionTree.ContainsKey(exc));
+    }
+}
+
