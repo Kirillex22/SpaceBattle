e03ee487cba4667b373ebcbb0a427f899273208d
Musiyak Egor <musiyak26@mail.ru>
2024-03-06 07:27:44 +0000

    add tests

--- SpaceBattle.Lib/Commands/StopServerCommand.cs
+++ SpaceBattle.Lib/Commands/StopServerCommand.cs
@@ -7,7 +7,7 @@ public class StopServerCommand : ICommand
 {
     public void Execute()
     {
-        IoC.Resolve<Dictionary<int, object>>("Server.Thread.Split").ToList().ForEach(i => 
+        IoC.Resolve<Dictionary<int, object>>("Server.Thread.Handle").ToList().ForEach(i => 
         IoC.Resolve<ICommand>("Server.Command.Send", i.Key, IoC.Resolve<ICommand>("Server.Thread.Stop", i.Key)).Execute());
     }
 }
--- SpaceBattle.Tests/CommandTests/ServerTests/StartServerCommandTests.cs
+++ SpaceBattle.Tests/CommandTests/ServerTests/StartServerCommandTests.cs
@@ -6,27 +6,25 @@ namespace SpaceBattle.Lib.Tests;
 
 public class StartServerCommandTests
 {
-    public StartServerCommandTests()
+    [Fact]
+    public void SuccefulStartServer()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", 
             IoC.Resolve<object>("Scopes.New", 
             IoC.Resolve<object>("Scopes.Root"))
         ).Execute();
-    }
 
-    [Fact]
-    public void SuccefulStartServer()
-    {
         int count = 5;
         var MoqCommand = new Mock<SpaceBattle.Lib.ICommand>();
 
         MoqCommand.Setup(i => i.Execute()).Verifiable();
 
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Resolve", "Server.Create.Thread", (object[] args) => MoqCommand.Object).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Resolve", "Server.Start", (object[] args) => new StartServerCommand((int)args[0])).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("Server.Start", count).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Server.Create.Thread", (object[] args) => MoqCommand.Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Server.Start", (object[] args) => new StartServerCommand((int)args[0])).Execute();
+        IoC.Resolve<SpaceBattle.Lib.ICommand>("Server.Start", count).Execute();
 
         MoqCommand.Verify(i => i.Execute(), Times.Exactly(count));
     }
 }
+
