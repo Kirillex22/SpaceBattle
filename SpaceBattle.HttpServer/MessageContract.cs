a70acf1a19271f52b6678eaa4247b40995c9ac78
Kirill <ziabrew34@gmail.com>
2024-04-17 08:19:15 +0000

    fixed

--- SpaceBattle.HttpServer/MessageContract.cs
+++ SpaceBattle.HttpServer/MessageContract.cs
@@ -5,5 +5,5 @@ public class MessageContract
     public string Type { get; set; } = "";
     public string GameId { get; set; } = "";
     public string ItemId { get; set; } = "";
-    public Dictionary<string, object> InitialValues { get; set; }
+    public Dictionary<string, object>? InitialValues { get; set; }
 }
