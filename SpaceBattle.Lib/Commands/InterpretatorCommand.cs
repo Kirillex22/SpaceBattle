93e8cb12634ff37e1c75ccb1d1628140b842ac64
Egor Musiyak <64477831+Egor767@users.noreply.github.com>
2024-04-17 06:02:16 +0000

    add files

---
+++ SpaceBattle.Lib/Commands/InterpretatorCommand.cs
@@ -0,0 +1,18 @@
+namespace SpaceBattle.Lib;
+
+public class InterpretatorCommand: ICommand
+{
+    IMassage _message;
+
+    public InterpretatorCommand(IMessage message)
+    {
+        _message = message;
+    }
+
+    public void Execute()
+    {
+        var iterpretatorCommand = IoC.Resolve<ICommand>("Game.Command.Create.", _message);
+        IoC.Resolve<ICommand>("Game.Queue.Push", _message.GameId, iterpretatorCommand).Execute();
+    }
+}
+
