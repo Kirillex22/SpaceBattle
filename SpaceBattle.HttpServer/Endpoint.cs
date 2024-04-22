28b77bd9d034f13f4c7b491bd1aa0390e0fe8417
Kirill <ziabrew34@gmail.com>
2024-04-22 11:31:54 +0000

    deleted return

--- SpaceBattle.HttpServer/Endpoint.cs
+++ SpaceBattle.HttpServer/Endpoint.cs
@@ -2,6 +2,7 @@ using System.Net.Http.Json;
 using System.Collections.Generic;
 using Hwdtech;
 using Microsoft.Extensions.ObjectPool;
+using System.Runtime.CompilerServices;
 
 namespace SpaceBattle.HttpServer;
 
@@ -15,13 +16,13 @@ public class Endpoint : IDisposable
         _scope = scope;
         var builder = WebApplication.CreateBuilder();
         _app = builder.Build();
+
         _app.MapPost("/send_message", (MessageContract message) =>
         {
             IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", _scope).Execute();
             var queueId = IoC.Resolve<string>("Endpoint.GetServerThreadQueueIdByGameId", message.GameId);
             var cmd = IoC.Resolve<SpaceBattle.Lib.ICommand>("Endpoint.ParseMessageToCmd", message);
             IoC.Resolve<SpaceBattle.Lib.ICommand>("ServerThread.SendCommand", cmd, queueId).Execute();
-            return message;
         });
 
     }
@@ -36,4 +37,3 @@ public class Endpoint : IDisposable
         _app.StopAsync();
     }
 }
-
