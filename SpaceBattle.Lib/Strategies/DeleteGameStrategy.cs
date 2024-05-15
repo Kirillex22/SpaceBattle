01d017efb4166b8549e80aabd223507f25f5e764
Egor Musiyak <64477831+Egor767@users.noreply.github.com>
2024-05-15 03:43:22 +0000

    add files

---
+++ SpaceBattle.Lib/Strategies/DeleteGameStrategy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using Hwdtech;
+using Hwdtech.Ioc;
+
+namespace SpaceBattle.Lib;
+
+public class DeleteGameStrategy
+{
+    public object Run(object[] args)
+    {
+        int gameId = (int)args[0];
+        return new DeleteGameCommand(gameId);
+    }
+}
+
