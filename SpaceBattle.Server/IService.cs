231f0af8d72f36edb9dea0ce36f9f2fb5ae9bff6
Кирилл <126565102+Kirillex22@users.noreply.github.com>
2024-05-29 05:10:04 +0000

    fixed coverlet

---
+++ SpaceBattle.Server/IService.cs
@@ -0,0 +1,17 @@
+using System.Net;
+using CoreWCF.OpenApi.Attributes;
+using CoreWCF.Web;
+
+namespace SpaceBattle.Server;
+
+[ServiceContract]
+[OpenApiBasePath("/api")]
+public interface IService
+{
+    [OperationContract]
+    [OpenApiResponse(ContentTypes = new[] { "application/json" }, Description = "Success", StatusCode = HttpStatusCode.OK)]
+    [OpenApiTag("Tag")]
+    [WebInvoke(Method = "POST", UriTemplate = "/send_message", RequestFormat = WebMessageFormat.Json)]
+    public void HandleMessage([OpenApiParameter(ContentTypes = new[] { "application/json" }, Description = "param description.")] MessageContract param);
+}
+
