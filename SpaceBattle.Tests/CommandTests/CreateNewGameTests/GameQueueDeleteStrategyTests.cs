6b0839f2d8b02ad213284318c41f77335a9ba325
Кирилл <126565102+Kirillex22@users.noreply.github.com>
2024-05-29 05:29:11 +0000

    fixes

--- SpaceBattle.Tests/CommandTests/CreateNewGameTests/GameQueueDeleteStrategyTests.cs
+++ SpaceBattle.Tests/CommandTests/CreateNewGameTests/GameQueueDeleteStrategyTests.cs
@@ -32,6 +32,23 @@ public class GameQueueDeleteStrategyTests
         IoC.Resolve<SpaceBattle.Lib.ICommand>("Game.Queue.Delete", 0).Execute();
 
         Assert.Empty(queue);
-    }   
+    }
+
+    [Fact]
+    public void EzTest()
+    {
+        var queueMap = new Dictionary<int, Queue<SpaceBattle.Lib.ICommand>>();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue.Delete", (object[] args) => new GameQueueDeleteStrategy().Run(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Get.Queue", (object[] args) => new GetGameQueueStrategy().Run(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue.Dict", (object[] args) => queueMap).Execute();
+        var scope = IoC.Resolve<object>("Scopes.Current");
+        var moqCmd = new Mock<SpaceBattle.Lib.ICommand>();
+        var queue = new Queue<SpaceBattle.Lib.ICommand>();
+
+        queue.Enqueue(moqCmd.Object);
+        queueMap.Add(0, queue);
+        Assert.ThrowsAny<Exception>(() => new PushQueueStrategy().Run(1, moqCmd.Object));
+        Assert.ThrowsAny<Exception>(() => new CreateNewScopeStrategy().Run(1, 1.0, scope));
+    }
 }
 
