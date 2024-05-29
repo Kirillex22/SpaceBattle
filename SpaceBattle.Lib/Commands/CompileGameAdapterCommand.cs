3ee33c70d36754a9cd806315ae93f7aa53718550
Egor Musiyak <64477831+Egor767@users.noreply.github.com>
2024-05-29 05:18:42 +0000

    files

---
+++ SpaceBattle.Lib/Commands/CompileGameAdapterCommand.cs
@@ -0,0 +1,26 @@
+using System;
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class CompileGameAdapterCommand : ICommand
+{
+    public Type _objectType;
+    public Type _targetType;
+
+    public CompileGameAdapterCommand(Type objectType, Type targetType)
+    {
+        _objectType = objectType;
+        _targetType = targetType;
+    }
+    
+    public void Execute()
+    {
+        var codeAdapter = IoC.Resolve<string>("Game.Code.Adapter", _objectType, _targetType);
+        var assembly = IoC.Resolve<Assembly>("Compile", codeAdapter);
+        var assemblyMap = IoC.Resolve<IDictionary<KeyValuePair<Type, Type>, Assembly>>("Game.Adapter.Assembly.Map");
+        var pair = new KeyValuePair<Type, Type>(_objectType, _targetType);
+
+        assemblyMap[pair] = assembly;
+    }
+}
